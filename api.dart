b2b118de509e2f831f45cb4146cd34641b279010
SudhaSusarita <32107057+SudhaSusarita@users.noreply.github.com>
2021-10-20 11:29:38 +0100

    Update api.dart

--- api.dart
+++ api.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 class Api{
 
    Future<dynamic> getDetails() async{
-    final response = await http.get("http://192.168.1.21:8080/displayList");
+    final response = await http.get("http:/");
     return json.decode(response.body);
   }
 }
